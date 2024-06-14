fde3fb893d73d617dd754ab43c07c3be3e58cc04
T Jake Luciani <jake@apache.org>
2024-06-14 16:25:44 -0400

    use arm style api and 128 bit versions

--- jlama-native/src/main/c/vector_simd.h
+++ jlama-native/src/main/c/vector_simd.h
@@ -10,24 +10,6 @@
 #define Q8_BLOCK_SIZE 32
 #define Q4_BLOCK_SIZE 32
 
-//F32
-float dot_product_f32(int flags, const float* a, int aoffset, const float* b, int boffset, int length);
-void dot_product_f32_chunked(int flags, float *r, int roffset, const float* a, int aoffset, const float* b, int boffset, int length, int bchunkstart, int bchunksize);
-void dot_product_f32_batch_chunked(int flags, int batch_num, void **r /*list of addresses*/, int roffset, const float* a, int aoffset, void **b /*list of addresses*/, int boffset, int length, int bchunkstart, int bchunksize);
-
-float dot_product_f32_q8(int flags, const float* a, int aoffset, const float *bf, const char* b, int boffset, int length);
-void dot_product_f32_q8_chunked(int flags, float *r, int roffset, const float* a, int aoffset, const float *bf, const char* b, int boffset, int length, int bchunkstart, int bchunksize);
-void dot_product_f32_q8_batch_chunked(int flags, int batch_num, void **r, int roffset, const float* a, int aoffset, const void **bf, const void **b, int boffset, int length, int bchunkstart, int bchunksize);
-
-float dot_product_f32_q4(int flags, const float *a, int aoffset, const float *bf, const char* b, int boffset, int length);
-void dot_product_f32_q4_chunked(int flags, float *r, int roffset, const float* a, int aoffset, const float *bf, const char* b, int boffset, int length, int bchunkstart, int bchunksize);
-void dot_product_f32_q4_batch_chunked(int flags, int batch_num, void **r, int roffset, const float* a, int aoffset, const void **bf, const void **b, int boffset, int length, int bchunkstart, int bchunksize);
-
-//I8
-float dot_product_q8(int flags, const float *af, const char* a, int aoffset, const float *bf, const char* b, int boffset, int length);
-float dot_product_q8_q4(int flags, const float *af, const char* a, int aoffset, const float *bf, const char* b, int boffset, int length);
-void dot_product_q8_q4_chunked(int flags, float *r, int roffset, const float* af, const char *a, int aoffset, const float *bf, const char* b, int boffset, int length, int bchunkstart, int bchunksize);
-void dot_product_q8_q4_batch_chunked(int flags, int batch_num, void **r, int roffset, const float* af, const char *a, int aoffset, const void **bf, const void **b, int boffset, int length, int bchunkstart, int bchunksize);
 
 //GEMM I8 Q4
 void gemm_q8_q4(int flags, const float * restrict af, const char* restrict a, int aoffset, const float * restrict bf, const char* restrict b, int boffset, float * restrict r, int roffset, int m, int n0, int n, int k, int lda, int ldaf, int ldb, int ldbf, int ldc);
--- jlama-tests/src/test/java/com/github/tjake/jlama/model/TestModels.java
+++ jlama-tests/src/test/java/com/github/tjake/jlama/model/TestModels.java
@@ -93,7 +93,7 @@ public class TestModels {
 
     @Test
     public void LlamaRun() throws Exception {
-        String modelPrefix = "../models/Llama-2-7b-chat-hf-jlama-Q4";
+        String modelPrefix = "../models/llama2-7b-chat-hf-jlama-Q4";
         Assume.assumeTrue(Files.exists(Paths.get(modelPrefix)));
         try (WeightLoader weights =
                 SafeTensorSupport.loadWeights(Path.of(modelPrefix).toFile())) {
